279a98b5a5c49a608f3dfc5966c7b26de78fd786
cod1r <jasonho353@gmail.com>
2022-01-23 23:35:10 -0600

    rewriting socket and signaling logic because I didn't realize how simple-peer worked

--- server.js
+++ server.js
@@ -13,12 +13,16 @@ io.on('connection', (socket) => {
 		socket.to(room).emit('newJoin', socket.id, username);
 
 		// for every socket in a particular room, they will generate another 'data' object to send to the joining peer so that,
-		// that peer can connect to the sending peers.
+		// that peer can generate an 'data offer' to connect to the sending peers.
 		// we do this because joining rooms will be less difficult as the joining peer won't have to know how many peers are already in the
-		// room and generate a certain amount of 'data' objects.
+		// room ( which can be inconsistent depending on the timing ) and generate a certain amount of 'data offer' objects.
 		// for the peers already in a room.
-		socket.on('data', (data, socketid) => {
-			socket.to(socketid).emit('dataConnect', data, username);
+		socket.on('dataOffer', (dataOffer, socketid) => {
+			socket.to(socketid).emit('dataOffer', dataOffer, socket.id, username);
+		});
+
+		socket.on('dataAnswer', (dataAnswer, socketid) => {
+			socket.to(socketid).emit('dataAnswer', dataAnswer);
 		});
 	});
 	socket.on('disconnect', (reason) => {
